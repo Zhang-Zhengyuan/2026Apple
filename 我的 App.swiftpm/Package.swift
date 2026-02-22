bba118e75dc65dd6592053650d440d32fcc76d36
zhangzhengyuan <zhangzy24@mails.tsinghua.edu.cn>
2026-02-22 22:51:35 +0800

    1714

--- 我的 App.swiftpm/Package.swift
+++ 我的 App.swiftpm/Package.swift
@@ -8,13 +8,13 @@ import PackageDescription
 import AppleProductTypes
 
 let package = Package(
-    name: "我的 App",
+    name: "Light of Life",
     platforms: [
         .iOS("18.1")
     ],
     products: [
         .iOSApplication(
-            name: "我的 App",
+            name: "Light of Life",
             targets: ["AppModule"],
             displayVersion: "1.0",
             bundleVersion: "1",
